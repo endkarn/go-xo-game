6b9b27a7e9c48fb9bec37bdcb3762a053a71fe72
endkarn <end_karn@hotmail.com>
2019-08-22 14:25:07 +0700

    [Added]: การทดสอบของกรณีที่ X ชนะในแนวแกนทแยงมุม

--- xo/xo_public_test.go
+++ xo/xo_public_test.go
@@ -97,3 +97,33 @@ func Test_XOGame_PlayerOne_Win_In_Vertical_ThirdLine(t *testing.T) {
 
 	assert.Equal(t,expected,actual)
 }
+
+func Test_XOGame_PlayerOne_Win_In_CrossLine_TopLeftToBottomRight(t *testing.T) {
+	expected := "X WIN"
+	playerOne := xo.NewPlayer("KA","X")
+	playerTwo := xo.NewPlayer("PK","O")
+	game := xo.NewGame(playerOne,playerTwo)
+
+	game.Play(playerOne,0,0)
+	game.Play(playerTwo,1,0)
+	game.Play(playerOne,1,1)
+	game.Play(playerTwo,2,0)
+	actual := game.Play(playerOne,2,2)
+
+	assert.Equal(t,expected,actual)
+}
+
+func Test_XOGame_PlayerOne_Win_In_CrossLine_TopRightToBottomLeft(t *testing.T) {
+	expected := "X WIN"
+	playerOne := xo.NewPlayer("KA","X")
+	playerTwo := xo.NewPlayer("PK","O")
+	game := xo.NewGame(playerOne,playerTwo)
+
+	game.Play(playerOne,0,2)
+	game.Play(playerTwo,1,0)
+	game.Play(playerOne,1,1)
+	game.Play(playerTwo,2,1)
+	actual := game.Play(playerOne,2,0)
+
+	assert.Equal(t,expected,actual)
+}
