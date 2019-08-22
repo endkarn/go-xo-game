d080b3a2b236150eacc07e5808b35af86d3b3ce7
endkarn <end_karn@hotmail.com>
2019-08-22 14:16:37 +0700

    [Added]: การทดสอบของกรณีที่ X ชนะในแนวแกนตั้ง ทั้งสามแบบ

--- xo/xo_public_test.go
+++ xo/xo_public_test.go
@@ -50,3 +50,50 @@ func Test_XOGame_PlayerOne_Win_In_Horizontal_ThirdLine(t *testing.T) {
 
 	assert.Equal(t,expected,actual)
 }
+
+func Test_XOGame_PlayerOne_Win_In_Vertical_FirstLine(t *testing.T) {
+	expected := "X WIN"
+	playerOne := xo.NewPlayer("KA","X")
+	playerTwo := xo.NewPlayer("PK","O")
+	game := xo.NewGame(playerOne,playerTwo)
+
+	game.Play(playerOne,0,0)
+	game.Play(playerTwo,0,1)
+	game.Play(playerOne,1,0)
+	game.Play(playerTwo,1,1)
+	actual := game.Play(playerOne,2,0)
+
+	assert.Equal(t,expected,actual)
+}
+
+
+func Test_XOGame_PlayerOne_Win_In_Vertical_SecondLine(t *testing.T) {
+	expected := "X WIN"
+	playerOne := xo.NewPlayer("KA","X")
+	playerTwo := xo.NewPlayer("PK","O")
+	game := xo.NewGame(playerOne,playerTwo)
+
+	game.Play(playerOne,0,1)
+	game.Play(playerTwo,2,2)
+	game.Play(playerOne,1,1)
+	game.Play(playerTwo,1,2)
+	actual := game.Play(playerOne,2,1)
+
+	assert.Equal(t,expected,actual)
+}
+
+
+func Test_XOGame_PlayerOne_Win_In_Vertical_ThirdLine(t *testing.T) {
+	expected := "X WIN"
+	playerOne := xo.NewPlayer("KA","X")
+	playerTwo := xo.NewPlayer("PK","O")
+	game := xo.NewGame(playerOne,playerTwo)
+
+	game.Play(playerOne,0,2)
+	game.Play(playerTwo,1,0)
+	game.Play(playerOne,1,2)
+	game.Play(playerTwo,2,0)
+	actual := game.Play(playerOne,2,2)
+
+	assert.Equal(t,expected,actual)
+}
