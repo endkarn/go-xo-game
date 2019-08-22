bf6aea6994e9dffbb2908121b070790a20925cf4
endkarn <end_karn@hotmail.com>
2019-08-22 11:36:54 +0700

    [Added]: เพิ่ม UnitTest ของ checkWin การมีผู้ชนะเป็น X และไม่มีผู้ชนะ

--- xo/xo_test.go
+++ xo/xo_test.go
@@ -1,8 +1,8 @@
 package xo
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func Test_XOGame_PlayerOne_Win_In_Horizontal_FirstLine(t *testing.T) {
@@ -32,6 +32,32 @@ func Test_Marking_Input_PlayerOne_LocationX_0_LocationY_0_Should_Be_X_In_Board_0
 
 	game.marking(playerOne,0,0)
 	actual := game.board
+
+	assert.Equal(t,expected,actual)
+}
+
+func Test_CheckWin_Input_XXX_In_First_Line_Should_Be_X_WIN(t *testing.T){
+	expected := "X WIN"
+	playerOne := NewPlayer("KA","X")
+	playerTwo := NewPlayer("PK","O")
+	game := NewGame(playerOne,playerTwo)
+	game.board = [3][3]string{
+		[3]string{"X","X","X"},
+		[3]string{"","",""},
+		[3]string{"","",""},
+	}
+	_,actual := game.checkWin()
+
+	assert.Equal(t,expected,actual)
+}
+
+func Test_CheckWin_Input_Board_From_NewGame_Should_Be_NO_WIN(t *testing.T){
+	expected := "NO WIN"
+	playerOne := NewPlayer("KA","X")
+	playerTwo := NewPlayer("PK","O")
+	game := NewGame(playerOne,playerTwo)
+
+	_,actual := game.checkWin()
 
 	assert.Equal(t,expected,actual)
 }
