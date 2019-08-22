d48c3a7d7273d68c87082d995955972d422f0519
endkarn <end_karn@hotmail.com>
2019-08-22 11:25:06 +0700

    [Added]: เพิ่ม UnitTest ของ marking ในตำแหน่งที่ 0,0 โดย X

--- xo/xo_test.go
+++ xo/xo_test.go
@@ -1,6 +1,9 @@
 package xo
 
-import "testing"
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_XOGame_PlayerOne_Win_In_Horizontal_FirstLine(t *testing.T) {
 	expected := "X WIN"
@@ -14,7 +17,21 @@ func Test_XOGame_PlayerOne_Win_In_Horizontal_FirstLine(t *testing.T) {
 	game.Play(playerTwo,1,1)
 	actual := game.Play(playerOne,0,2)
 
-	if expected != actual {
-		t.Errorf("Expected %s but get %s",expected,actual)
+	assert.Equal(t,expected,actual)
+}
+
+func Test_Marking_Input_PlayerOne_LocationX_0_LocationY_0_Should_Be_X_In_Board_0_0(t *testing.T){
+	expected := [3][3]string{
+		[3]string{"X","",""},
+		[3]string{"","",""},
+		[3]string{"","",""},
 	}
+	playerOne := NewPlayer("KA","X")
+	playerTwo := NewPlayer("PK","O")
+	game := NewGame(playerOne,playerTwo)
+
+	game.marking(playerOne,0,0)
+	actual := game.board
+
+	assert.Equal(t,expected,actual)
 }
