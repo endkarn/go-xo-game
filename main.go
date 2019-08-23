7e860cee8c04747b4c5a5aaf92df016c7eda28c2
endkarn <end_karn@hotmail.com>
2019-08-23 12:21:06 +0700

    [Added]: เพิ่ม route API ของ ViewGameHandler ใน main.go

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"go-xo-game/api"
 )
 
-func main(){
+func main() {
 	host := flag.String("host", "3000", "start api host port")
 	flag.Parse()
 	route := gin.Default()
-	route.POST("/new_game",api.NewGameHandler)
-	route.POST("/play", api.PlayHandler)
-	route.Run(":"+*host)
+	gameAPI := api.GameAPI{}
+	route.POST("/new_game", gameAPI.NewGameHandler)
+	route.POST("/play", gameAPI.PlayHandler)
+	route.GET("/game", gameAPI.ViewGameHandler)
+	route.Run(":" + *host)
 }
