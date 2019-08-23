90393f5b25876ad754ed58dda63972ed6072022f
endkarn <end_karn@hotmail.com>
2019-08-23 11:30:33 +0700

    [Added]: สร้าง PlayGameHandler สำหรับเล่นเกมของผู้เล่น

--- api/api.go
+++ api/api.go
@@ -6,6 +6,14 @@ import (
 	"net/http"
 )
 
+var xoGame xo.Game
+
+type PlayGame struct {
+	Player xo.Player `json:"player"`
+	LocationX int `json:"location_x"`
+	LocationY int `json:"location_y"`
+}
+
 func NewGameHandler(context *gin.Context){
 	var reqXOGame xo.Game
 	err := context.BindJSON(&reqXOGame)
@@ -15,6 +23,18 @@ func NewGameHandler(context *gin.Context){
 	}
 	playerOne := xo.NewPlayer(reqXOGame.PlayersOne.Name,reqXOGame.PlayersOne.Symbol)
 	playerTwo := xo.NewPlayer(reqXOGame.PlayersTwo.Name,reqXOGame.PlayersTwo.Symbol)
-	xoGame := xo.NewGame(playerOne,playerTwo)
+	xoGame = xo.NewGame(playerOne,playerTwo)
 	context.JSON(http.StatusCreated, xoGame)
+}
+
+func PlayHandler(context *gin.Context){
+	var reqPlayGame PlayGame
+	err := context.BindJSON(&reqPlayGame)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{
+		"message":xoGame.Play(reqPlayGame.Player,reqPlayGame.LocationX,reqPlayGame.LocationY),
+	})
 }
