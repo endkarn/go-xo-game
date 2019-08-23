4916fd5a67187386bc41ad77256f9553b306a97e
endkarn <end_karn@hotmail.com>
2019-08-23 11:03:55 +0700

    [Edtied]: แก้ไขชื่อ TestCase ให้ตรงกับชื่อฟังชั่น NewGame

--- api/api_test.go
+++ api/api_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func Test_CreateXOGame_Input_PlayerOne_KA_PlayerTwo_PK_Should_Be_New_XOGame_With_Two_Players_Empty_Board_And_No_Score(t *testing.T){
+func Test_CreateNewXOGame_Input_PlayerOne_KA_PlayerTwo_PK_Should_Be_New_XOGame_With_Two_Players_Empty_Board_And_No_Score(t *testing.T){
 	expected := `{"board":[["","",""],["","",""],["","",""]],"player_one":{"symbol":"X","name":"KA","score":0},"player_two":{"symbol":"O","name":"PK","score":0},"current_turn":"KA"}`
 	requestBody := `{"player_one":{"symbol":"X","name":"KA"},"player_two":{"symbol":"O","name":"PK"}}`
 	request := httptest.NewRequest("POST","/new_game",strings.NewReader(requestBody))
